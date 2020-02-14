4bcd4af513b00d7447750b3241e30aabfb662d80
Anna Karutina <anna.karutina@khk.ee>
2020-02-14 13:15:44 +0200

    Lisatud tagasi mineku nupp

--- app/views/posts/show.php
+++ app/views/posts/show.php
@@ -1,8 +1,13 @@
 <?php require_once APPROOT.'/views/inc/header.php'; ?>
+<a href="<?php echo URLROOT?>/posts" class="btn btn-info">Back</a>
 <h1><?php echo $data['post']->title; ?></h1>
 <div class="bg-secondary text-white p-2 mb-3">
   Created by <?php echo $data['post']->user_id;?> at <?php echo $data['post']->created_at;?>
 </div>
 <p><?php echo $data['post']->content; ?></p>
-<a href="<?php echo URLROOT?>/posts" class="btn btn-info">Back</a>
+<hr>
+<?php if($data['post']->user_id == $_SESSION['user_id']) :?>
+  <a href="<?php echo URLROOT?>/posts/edit/<?php echo $data['post']->id; ?>" class="btn btn-success">Edit</a>
+  <a href="<?php echo URLROOT?>/posts/delete/<?php echo $data['post']->id; ?>" class="btn btn-danger">Delete</a>
+<?php endif;?>
 <?php require_once APPROOT.'/views/inc/footer.php'; ?>
